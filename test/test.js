3c426b1f998dfd1d4a630ed8a686abe5b1830634
Patrick O'Doherty <p@trickod.com>
2012-11-30 18:00:58 -0800

    Added testing for the memory_store Now to make it generic and able to be passed any type of store

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var _ = require('underscore');
 var MemoryStore = require('../lib/memory_store');
 
 describe('MemoryStore', function(){
+  var memory_store = new MemoryStore();
   describe('#init', function(){
     it('should create an empty store by default', function() {
       var memory_store = new MemoryStore();
@@ -11,20 +12,50 @@ describe('MemoryStore', function(){
     });
   });
   describe('#get', function() {
-    var memory_store = new MemoryStore();
-
-    it('should return null when no data exists for key', function(done) {
+    it('should return null when no data exists for key', function() {
       memory_store.read('test-key', function(err, value) {
-        console.log(arguments);
         if (err) throw err;
-        value.should.equal(null);
+        should.not.exist(value);
       })
     });
 
-    it('should write values into the store when passed a generator', function(done) {
+    it('should write values into the store when passed a generator', function() {
       memory_store.read('test-key', function(cb){ cb(null, 'value')}, function(err, value) {
         if (err) return done(err);
-        done(null, (value == 'value'));
+        value.should.equal('value');
+      });
+    });
+  });
+  describe('#remove', function() {
+    it('should remove values when given key', function() {
+      memory_store.remove('test-key', function(){
+        memory_store.read('test-key', function(err, value){
+          should.not.exist(value)
+        });
+      });
+    });
+  });
+  describe('#write', function(){
+    it('should write values for given key', function(done){
+      memory_store.write('key', 'value', done);
+    })
+  });
+  describe('#exists', function(){
+    it('should return true for existing values', function() {
+      memory_store.exists('key', function(err, result) {
+        result.should.equal(true);
+      });
+    });
+    it('should return false for missing values', function(){
+      memory_store.exists('missing-key', function(err, result) {
+        result.should.equal(false);
+      });
+    });
+  });
+  describe('#clear', function(){
+    it('should empty the store of all values', function() {
+      memory_store.clear(function(){
+        _.keys(memory_store.data).length.should.equal(0);
       });
     });
   });
