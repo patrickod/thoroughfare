28e22adfc13d7cb16fd913503b4099f2d95ddb43
Patrick O'Doherty <p@trickod.com>
2012-12-02 11:03:55 -0800

    Re-factored the test.js main file. No more async needed

--- test/test.js
+++ test/test.js
@@ -6,24 +6,9 @@ var utils = require('./utils');
 var MemoryStore = require('../lib/memory_store');
 var RedisStore = require('../lib/redis_store');
 
-describe('Stores', function(){
-  var tests = {};
-
-  before(function(done){
-    async.parallel({
-      MemoryStore: function(cb) { new MemoryStore({}, cb)},
-      RedisStore: function(cb) { new RedisStore({}, cb)}
-    },
-    function(err, results) {
-      if (err) return console.error(err);
-      _.map(_.keys(results), function(key){
-        utils.run_tests(key, results[key])
-      })
-      done();
-    });
-  });
-  it('should be true', function(){
-    var t = true;
-    t.should.equal(true);
-  });
+new MemoryStore({}, function(err, store) {
+  utils.run_tests('MemoryStore', store)
+});
+new RedisStore({}, function(err, store) {
+  utils.run_tests('RedisStore', store);
 });
